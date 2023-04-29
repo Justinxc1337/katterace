26d92c834ef372e7ae9d6bafa9c4f2b9a970ed37
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-04-29 05:39:50 +0200

    indexment

--- src/main/java/com/perser/katterace/KatteraceApplication.java
+++ src/main/java/com/perser/katterace/KatteraceApplication.java
@@ -2,12 +2,34 @@ package com.perser.katterace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
+@RestController
+@Controller
 public class KatteraceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KatteraceApplication.class, args);
+	}
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/kontaktos")
+	public String kontaktos() {
+		return "kontaktos";
 	}
 
 }
--- src/test/java/com/perser/katterace/KatteraceApplicationTests.java
+++ src/test/java/com/perser/katterace/KatteraceApplicationTests.java
@@ -3,6 +3,7 @@ package com.perser.katterace;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class KatteraceApplicationTests {
 
