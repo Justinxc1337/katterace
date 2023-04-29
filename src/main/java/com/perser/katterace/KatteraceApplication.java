37fa5ee5f8386508893c298c50e7b91b07e4aac0
Justinxc1337 <110351655+Justinxc1337@users.noreply.github.com>
2023-04-29 04:22:44 +0200

    123

---
+++ src/main/java/com/perser/katterace/KatteraceApplication.java
@@ -0,0 +1,13 @@
+package com.perser.katterace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatteraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatteraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perser/katterace/KatteraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perser.katterace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatteraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
